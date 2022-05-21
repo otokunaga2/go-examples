8f8db0de589d7deb57aa081dcda5f8ee3900b837
Seiki Tokunaga <seiki.tokunaga@riken.jp>
2022-05-21 17:24:17 +0900

    Refactor roman function

--- tdd/property_based_test/roman.go
+++ tdd/property_based_test/roman.go
@@ -1,12 +1,14 @@
 package main
+
 import "strings"
 
-func ConvertToRoman(arabic int) string{
-	if arabic == 4 {
-		return "IV"
-	}
+func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for i:=0; i<arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()

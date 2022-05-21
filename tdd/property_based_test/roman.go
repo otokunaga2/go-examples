8ece6adf110edf7700bcea6adf189ae6cf85d35c
Seiki Tokunaga <seiki.tokunaga@riken.jp>
2022-05-21 19:14:17 +0900

    Refactor

--- tdd/property_based_test/roman.go
+++ tdd/property_based_test/roman.go
@@ -4,12 +4,19 @@ import "strings"
 
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for i := arabic; i > 0; i-- {
-		if i == 4 {
-			result.WriteString("IV")
-			break
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+				result.WriteString("V")
+				arabic -= 5
+		case arabic > 3:
+				result.WriteString("IV")
+				arabic -= 4
+
+		default:
+			result.WriteString("I")
+			arabic--
 		}
-		result.WriteString("I")
 	}
 	return result.String()
 }

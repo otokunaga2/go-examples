f0e9b6abb49118c8cb17a63ecb609b0ad9eefb4d
Seiki Tokunaga <seiki.tokunaga@riken.jp>
2022-05-21 17:14:57 +0900

    Passed test case (4)

--- tdd/property_based_test/roman.go
+++ tdd/property_based_test/roman.go
@@ -2,6 +2,9 @@ package main
 import "strings"
 
 func ConvertToRoman(arabic int) string{
+	if arabic == 4 {
+		return "IV"
+	}
 	var result strings.Builder
 	for i:=0; i<arabic; i++ {
 		result.WriteString("I")

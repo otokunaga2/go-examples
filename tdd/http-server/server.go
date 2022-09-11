0f8814351864468a446f98404b5da6e88858643b
Seiki Tokunaga <otokuna46@gmail.com>
2022-09-11 17:57:17 +0900

    Implement player's calcurate score logic (tentative)

--- tdd/http-server/server.go
+++ tdd/http-server/server.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func ListenAndServe(addr string, handler Handler) error {
 	return nil
 }
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	} else if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+
+	}
 }
 
 type Handler interface {
--- tdd/http-server/server_test.go
+++ tdd/http-server/server_test.go
@@ -21,4 +21,19 @@ func TestGETPlayers(t *testing.T) {
 
 		}
 	})
+
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+
+		}
+	})
 }
